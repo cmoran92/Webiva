0da4465ea89d3f9be8e1b5efecbe483d3d749297
Carlos Moran <cmoran92@gmail.com>
2012-02-29 01:22:21 +1100

    Complain if can't send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors if you bad email addresses should just be ignored
-# config.action_mailer.raise_delivery_errors = false
+# Enable delivery errors 
+config.action_mailer.raise_delivery_errors = true
 
 config.action_mailer.delivery_method = :sendmail
 config.action_mailer.perform_deliveries = true
